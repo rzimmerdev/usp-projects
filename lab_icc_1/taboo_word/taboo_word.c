870bed21b10d841891be17c85696fac6b5fafc7f
Rafael Zimmer <rzimmerdev@gmail.com>
2021-07-16 01:47:47 -0300

    taboo word

--- lab_icc_1/taboo_word/taboo_word.c
+++ lab_icc_1/taboo_word/taboo_word.c
@@ -2,14 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
+char *read_line();
 
-	char taboo_word[20];
-	scanf("%s\n", taboo_word);
+int main () {
 
-	char *phrase = calloc(0, sizeof(char));
+	char taboo[20];
+	scanf("%s\n", taboo);
 
-	printf("%d", strcmp(taboo_word, "demacia"));
-	free(phrase);
-	return 0;
+	char *phrase = read_line();
+	char *result_phrase = phrase;
+
+	while (result_phrase != NULL) {
+
+		memmove(result_phrase, result_phrase + strlen(taboo), strlen(result_phrase + strlen(taboo)) + 1);
+		result_phrase = strstr(result_phrase, phrase);
+	}
+
+	printf("The substring is: %s\n", result_phrase);
+
+	return(0);
+}
+
+char *read_line() {
+
+	// Declare line array, with dynamic size
+	char *line = NULL;
+	char current_char = -1;
+
+	int index = 0, string_size = 0;
+
+	// Keep reading until current character is NULL character (set by program)
+	while (current_char) {
+
+		// Scan character and see if is EOF or \n, then stop reading line and add null character to end of string
+		if (!scanf("%c", &current_char) || current_char == '$' || current_char == '\n') {
+			current_char = 0;
+		}
+
+		// Increment size only if increment does not overflow current indexing of characters
+		if (string_size++ <= index) {
+			line = ( char* ) realloc(line, string_size);
+
+		}
+
+		*(line + index++) = current_char;
+
+	}
+
+	// Return pointer to dynamically allocated string
+	return line;
 }

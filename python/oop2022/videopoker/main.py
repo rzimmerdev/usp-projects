9fd2a48cb6fcda717921f9ce2fd69502ad92da98
Rafael Zimmer <rzimmerdev@gmail.com>
2022-05-05 15:55:23 -0300

    [ oop2022 ] - Working on Video Poker

--- python/oop2022/videopoker/main.py
+++ python/oop2022/videopoker/main.py
@@ -1,67 +1,18 @@
-import random
+from poker import Player
 
 
 def read_choice(parameters: str) -> list:
     return [int(x) if x.isdigit() else 0 for x in parameters.split()]
 
 
-class Card:
-    def __init__(self, maximum=51):
-        self._maximum = maximum
-        self._value = -1
-
-    def roll(self):
-        self._value = random.randint(0, self._maximum - 1)
-
-    def get_value(self):
-        return self._value
-
-    def __str__(self):
-        default_drawing = [
-            "{pos0}  {pos1}  {pos2}",
-            "{pos3}  {pos3}  {pos3}",
-            "{pos4}  {pos4}  {pos5}"
-        ]
-
-        special_drawing = [
-            ["    {symbol}  ",
-             "   ♘   ",
-             "  {symbol}    "],
-            ["    {symbol}  ",
-             "   ♕   ",
-             "  {symbol}    "],
-            ["    {symbol}  ",
-             "   ♔   ",
-             "  {symbol}    "],
-            ["    {symbol}  ",
-             "    As   ",
-             "  {symbol}    "],
-        ]
-
-        card = ("┌───────────┐"
-                "│{value}          │"
-                "│  {draw0}  │"
-                "│           │"
-                "│  {draw1}  │"
-                "│           │"
-                "│  {draw2}  │"
-                "│          {value}│"
-                "└──────────┘")
-
-        if int((self._value % 13) > 7):
-            return [line.format(int(self._value / 13)) for line in special_drawing[(self._value % 13) - 7]]
-
-        return [line.format(int(self._value / 13)) for line in default_drawing[(self._value % 13) - 7]]
-
-
-def player():
-    pass
-
-
 def main():
-    a = Card()
-    a.roll()
-    print(a)
+
+    original_deck = list(range(52))
+    player = Player(original_deck, hand_size=5)
+
+    deck = list(set(original_deck) - set(card.__int__() for card in player.get_hand()))
+
+
 
 
 if __name__ == "__main__":

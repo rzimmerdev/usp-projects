c76515aade48b72d29cd00e56c8aaf488000311e
Rafael Zimmer <rzimmerdev@gmail.com>
2022-07-07 23:22:09 -0300

    Finished Assignment 6 for Image Processing class

--- python/pdi/A6/main.py
+++ python/pdi/A6/main.py
@@ -54,67 +54,81 @@ def euclidean(first: np.ndarray, last: np.ndarray, axis: int = None):
 def grayscale(image: np.ndarray) -> np.ndarray:
     """Uses luminance weights to transform RGB channel to greyscale, by
     taking the dot product between the channel and the weights."""
-    return np.dot(image[:, :, 0:3], [0.299, 0.587, 0.114]).astype(np.uint8)
+    return np.dot(image[:, :, 0:3], [0.299, 0.587, 0.114])
 
 
 def initialize_clusters(image, total_clusters):
+    """Creates an array of random ids within given flattened image size,
+    for using when instantiating new centroids values."""
     size = image.shape[0] * image.shape[1]
     ids = np.sort(random.sample(range(0, size), total_clusters))
     return ids
 
 
-def map_distances(centroids, flatten, channels):
+def map_distances(centroids, flatten, params):
+    """Calculates the distance of each pixel within the flattened image to each centroid,
+    taking into account the desired vector parameters to use"""
     distances_map = np.zeros((flatten.shape[0], len(centroids)), dtype=np.float32)
 
     for idx, centroid in enumerate(centroids):
-        distance_to_centroid = euclidean(flatten[:, 0:channels], flatten[:, 0:channels][int(centroid)], axis=1)
+        distance_to_centroid = euclidean(flatten[:, 0:params], centroid, axis=1)
         distances_map[:, idx] = distance_to_centroid
 
     labels_mask = np.argmin(distances_map[:, :], axis=1)
     return labels_mask
 
 
-def kneighbours_classifier(image, total_clusters, total_iterations, channels):
+def kneighbours_classifier(image, total_clusters, total_iterations, image_type):
     """Generates a label mask for the given image.
     Uses the kneareset neighbours algorithm for performing classification on the given image.
     Resulting labeled mask contains the selected k number of labeled groups.
 
     Args:
-        image:
-        total_clusters:
-        total_iterations:
+        image: Original image to perform nearest neighbours segmentation.
+        total_clusters: How many centroids should the image be separated into.
+        total_iterations: Total convergence steps to be taken when recalculating centroids
+        image_type: Original image type, defining which color code to use, as well as which encoding,
+                    being either (RGB; RGB + X,Y; L; L + X, Y).
     Returns:
         Label mask for the given image, with clusters number of different groups.
     """
-    centroids = initialize_clusters(image, total_clusters)
+    # Vectorize image and initialize centroids with random indices
     flatten = image.reshape((image.shape[0] * image.shape[1], -1))
+    centroids = flatten[initialize_clusters(image, total_clusters)]
 
-    shape = image.shape[0] * image.shape[1]
+    # Decide how many color channels the image has based on its type,
+    # as well as which encoding to use (only color channels or use coordinates as well).
+    channels = 3 if image_type in {1, 2} else 1
+    params = channels
+    if image_type in {2, 4}:
+        params += 2
 
-    for i in range(total_iterations):
+    for i in range(total_iterations - 1):
         # For each centroid, calculate the distance between its channels and each individual pixel within
         # the flattened image. Then, attribute to each position the id of the centroid to which the current pixel
         # has the smallest distance.
         # centroids: array with position of each centroid
         # flatten: flattened original image
-        labels_mask = map_distances(centroids, flatten, channels)
+        labels_mask = map_distances(centroids, flatten, params)
 
         for idx in range(len(centroids)):
-            positions = np.argwhere(labels_mask == idx)
-            if positions.size != 0:
-                centroids[idx] = np.mean(positions)
+            values = flatten[np.argwhere(labels_mask == idx)]
+            if values.size != 0:
+                centroids[idx] = np.mean(values, axis=1).mean(axis=0)
 
-    labels_mask = map_distances(centroids, flatten, channels)
+    labels_mask = map_distances(centroids, flatten, params)
 
-    colors = flatten[centroids][:, 0:channels]
-
+    # Normalize each color channel within the centroids
+    colors = centroids[:, 0:channels]
     for channel in range(channels):
         colors[:, channel] = normalize_array(colors[:, channel], 255)
 
+    # Then, assing to each pixel within the original image
+    # its color based on the centroid to which it is closest
     labels = labels_mask.reshape((image.shape[0], image.shape[1], -1))
-
-    for idx, centroid in enumerate(centroids):
+    for idx in range(len(centroids)):
         labels = np.where(labels == idx, colors[idx], labels)
+
     return labels.astype(np.float32)
 
 
@@ -134,15 +148,31 @@ def main():
 
     # Seed to be used for the random centroids
     seed = int(input())
+    random.seed(seed)
 
     input_image = imageio.imread(input_filename).astype(np.float64)
     reference_image = imageio.imread(reference_filename).astype(np.float64)
 
-    random.seed(seed)
+    # If image is of type gray, convert to grayscale using Luminance formulae
+    # Then, add an empty third dimension to use with generic KNN function
+    if image_type in {3, 4}:
+        input_image = grayscale(input_image).reshape((input_image.shape[0], input_image.shape[1], 1))
+        reference_image = reference_image.reshape((input_image.shape[0], input_image.shape[1], 1))
 
-    labeled_image = kneighbours_classifier(input_image, total_clusters, total_iterations, 3 if image_type < 3 else 1)
-    # show_images([labeled_image.astype(np.uint8), reference_image.astype(np.uint8)])
-    print(rmse(labeled_image, reference_image))
+    # If image is of type channels + coordinates (RGB + X,Y or L + X,Y)
+    # append the axis values to each channel value
+    if image_type in {2, 4}:
+        i, j = np.indices(input_image.shape[0:2])
+        input_image = np.append(input_image, i[:, :, np.newaxis], axis=2)
+        input_image = np.append(input_image, j[:, :, np.newaxis], axis=2)
+    labeled_image = kneighbours_classifier(input_image, total_clusters, total_iterations, image_type)
+
+    # Print error as a sum of errors within all color channels
+    channels = 3 if image_type in {1, 2} else 1
+    error = 0
+    for color in range(channels):
+        error += rmse(labeled_image[:, :, color], reference_image[:, :, color])
+    print(error)
 
 
 if __name__ == "__main__":
--- python/pdi/A6/view.py
+++ python/pdi/A6/view.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def show_images(*args, cmap=None):
     for image in args[0]:
         plt.figure(figsize=(16, 16))
-        if cmap:
+        if cmap is not None:
             plt.imshow(image, cmap=cmap)
         else:
             plt.imshow(image)

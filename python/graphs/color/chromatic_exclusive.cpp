911916ae76e33eec7df865cf9b29625585aff5d8
rzimmerdev <rzimmerdev@gmail.com>
2022-07-16 01:57:34 -0300

    Added DFS

--- python/graphs/color/chromatic_exclusive.cpp
+++ python/graphs/color/chromatic_exclusive.cpp
@@ -19,40 +19,55 @@ void print_adjacency(vector<int> adjacency[], int total_nodes) {
     }
 }
 
+
+int dfs(vector<int> adjacency[], int total_nodes, vector<int> visited, int node) {
+
+    if (visited[node] == true)
+        return 0;
+
+    else {
+        int depth = 0;
+        visited[node] = true;
+
+        for (int neighbour : adjacency[node]) {
+
+            int range = 1 + dfs(adjacency, total_nodes, visited, neighbour);
+            depth = range > depth ? range : depth;
+        }
+        printf("Node: %d, depth: %d\n", node, depth);
+
+        return depth;
+    }
+}
+
+
 int main() {
-
-    string filename;
-    getline(cin, filename);
-
-    ifstream pajek(filename);
-
-    string neighbors;
-    getline(cin, neighbors);
-
-    vector<int> sub_group;
-
-    istringstream in(neighbors);
-    copy(istream_iterator<int>( in ), std::istream_iterator<int>(), back_inserter(sub_group) );
+    vector<int> sub_group = {4, 5, 7, 9, 12, 13, 16, 17, 18, 19, 20};
 
     string empty;
 
-    pajek >> empty;
-    int total_vertices; pajek >> total_vertices;
-    pajek >> empty;
+    cin >> empty;
+    int total_vertices; cin >> total_vertices;
+    total_vertices += 1;
+    cin >> empty;
 
     vector<int> adjacency[total_vertices];
 
     for (int i = 0; i < total_vertices; i++) {
         int start, end;
-        pajek >> start >> end;
-        adjacency[start - 1].push_back(end);
+        cin >> start >> end;
+        if ((count(sub_group.begin(), sub_group.end(), end)))
+            adjacency[start].push_back(end);
     }
 
     for (int node : sub_group) {
         cout << "Starting Node " << node << ": \n";
-        for (auto neighbor : adjacency[node - 1])
+        for (auto neighbor : adjacency[node]) {
             cout << neighbor << " ";
-
-        printf("\n");
+        }
+        cout << "\n";
     }
+
+    vector<int> visited(total_vertices);
+    cout << dfs(adjacency, total_vertices, visited, 4);
 }

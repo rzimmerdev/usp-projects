dbed8a658623d7bdb8ec2f860ed430d339667c7f
Rafael Zimmer <rzimmerdev@gmail.com>
2022-09-01 17:25:05 -0300

    Finished the sprinkler problem

--- c++/advanced_algorithms/sprinklers/sprinklers.cpp
+++ c++/advanced_algorithms/sprinklers/sprinklers.cpp
@@ -5,72 +5,62 @@
 
 using namespace std;
 
-bool compare_start(const pair<double, double> &a, const pair<double, double> &b) {
-    return (a.first < b.first);
-}
-
 pair<double, double> get_range(double x, double r, double w) {
-
+    /*
+     * Converts a circular range element into its respective useful range,
+     * using the Pythagorean Theorem.
+     */
     double half_range = sqrt(pow(r, 2) - pow(w / 2, 2));
     return make_pair(x - half_range, x + half_range);
 }
 
 bool has_overlay(pair<double, double> first_range, pair<double, double> second_range) {
-
-    if (first_range.first < second_range.first) {
-
+    /*
+     * Returns the comparison if two sprinklers overlay their useful range.
+     */
+    if (first_range.first < second_range.first)
         return second_range.first < first_range.second;
-    }
-
-    else {
+    else
         return first_range.first < second_range.second;
-    }
 }
 
 int count_sprinklers(vector<pair<double, double>> sprinklers, int num_sprinklers, double strip_length) {
+    /*
+     * Counts how many useful sprinklers are needed to totally cover a desired strip, with given length.
+     */
     int count = 0;
 
-    int current_sprinkler = 0, next_sprinkler = 1, best_sprinkler = 1;
+    int current_sprinkler = 0, next_sprinkler = 1, best_sprinkler = -1;
     double best_distance = -1;
 
-    if (num_sprinklers <= 0 || sprinklers[0].first > 0 || sprinklers[num_sprinklers - 1].second < strip_length)
+    if (num_sprinklers <= 0 || sprinklers[0].first > 0)
         return -1;
 
+    // While there are currently any sprinklers left to iterate through, and the area is still not covered
     while (current_sprinkler < num_sprinklers) {
 
-        while (next_sprinkler < num_sprinklers && has_overlay(sprinklers[current_sprinkler], sprinklers[next_sprinkler])) {
-
+        // Iterate over sprinklers that overlay the currently selected sprinkler
+        while (next_sprinkler < num_sprinklers && has_overlay(sprinklers[current_sprinkler],
+                                                              sprinklers[next_sprinkler])) {
             if (best_distance < sprinklers[next_sprinkler].second) {
                 best_distance = sprinklers[next_sprinkler].second;
                 best_sprinkler = next_sprinkler;
             }
-
             next_sprinkler++;
         }
 
-        if (next_sprinkler >= num_sprinklers) {
-            current_sprinkler = best_sprinkler;
-            break;
-        }
+        // Jump to next best sprinkler decided beforehand, increase count and continue
+        // If the next best sprinkler also fills the area correctly, end counting
+        count++;
         if (current_sprinkler == best_sprinkler)
             return -1;
+
         current_sprinkler = best_sprinkler;
+        if (next_sprinkler >= num_sprinklers || sprinklers[current_sprinkler].second >= strip_length)
+            break;
     }
 
-    if (sprinklers[current_sprinkler].second < strip_length)
-        return -1;
-    return 1;
-}
-
-
-void print_sprinklers(vector<pair<double, double>> sprinklers, int num_sprinklers) {
-
-    for (int i = 0; i < num_sprinklers; i++)
-        cout << sprinklers[i].first << " ";
-    cout << "\n";
-    for (int i = 0; i < num_sprinklers; i++)
-        cout << sprinklers[i].second << " ";
-    cout << "\n\n";
+    return (sprinklers[current_sprinkler].second < strip_length) ? -1: count;
 }
 
 
@@ -80,13 +70,15 @@ int main() {
     double strip_length, strip_width;
     double sprinkler_pos, sprinkler_radius;
 
+    // Go over all test cases, until no other input can be read
     while (cin >> num_sprinklers) {
         cin >> strip_length >> strip_width;
 
-        vector<pair<double, double>> sprinklers(num_sprinklers);
+        vector<pair<double, double>> sprinklers;
 
         int useless_sprinklers = 0;
 
+        // Read each individual sprinkler from input line
         for (int i = 0; i < num_sprinklers; i++) {
 
             cin >> sprinkler_pos >> sprinkler_radius;
@@ -95,12 +87,15 @@ int main() {
                 useless_sprinklers++;
                 continue;
             }
-            sprinklers[i - useless_sprinklers] = get_range(sprinkler_pos, sprinkler_radius, strip_width);
+            sprinklers.push_back(get_range(sprinkler_pos, sprinkler_radius, strip_width));
         }
 
-        num_sprinklers -= useless_sprinklers;
-        sprinklers.resize(num_sprinklers);
-        sort(sprinklers.begin(), sprinklers.end(), compare_start);
+        // Add an empty sprinkler to ignore sprinklers that are not within the acceptable range
+        sprinklers.emplace_back((double) -1e18, 0);
+
+        num_sprinklers = (int) sprinklers.size();
+
+        sort(sprinklers.begin(), sprinklers.end());
         cout << count_sprinklers(sprinklers, num_sprinklers, strip_length) << "\n";
     }
 

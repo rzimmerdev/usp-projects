2a701cc8bc183e36d1cfdd07ca317d975037e42c
Rafael Zimmer <rzimmerdev@gmail.com>
2022-08-31 22:53:33 -0300

    Working on Gul 1 problem

--- c++/advanced_algorithms/sprinklers/sprinklers.cpp
+++ c++/advanced_algorithms/sprinklers/sprinklers.cpp
@@ -5,14 +5,13 @@
 
 using namespace std;
 
-//bool sort_end(const pair<double, double> &a, const pair<double, double> &b) {
-//    return (a.second < b.second);
-//}
+bool compare_start(const pair<double, double> &a, const pair<double, double> &b) {
+    return (a.first < b.first);
+}
 
 pair<double, double> get_range(double x, double r, double w) {
 
     double half_range = sqrt(pow(r, 2) - pow(w / 2, 2));
-
     return make_pair(x - half_range, x + half_range);
 }
 
@@ -32,48 +31,75 @@ int count_sprinklers(vector<pair<double, double>> sprinklers, int num_sprinklers
     if (sprinklers[0].first > 0 || sprinklers[num_sprinklers - 1].second < strip_length)
         return -1;
 
-    int current = 0, count = 0;
-    double pos = 0;
+    int count = 1;
+    if (sprinklers[0].first <= 0 && sprinklers[0].second >= strip_length)
+        return count;
 
-    while (pos < strip_length || current >= num_sprinklers) {
+    int current = 0;
+    double pos = -1;
 
-        int last = 0;
+    while (current < num_sprinklers) {
+        int last = current;
 
-        while (has_overlay(sprinklers[current], sprinklers[current + 1])) {
-            last = current;
-            current++;
+        while (last + 1 < num_sprinklers && has_overlay(sprinklers[last + 1], sprinklers[current])) {
+            if (sprinklers[last + 1].second >= strip_length)
+                return count + 1;
+            last++;
+
+            if (pos < sprinklers[last].second) {
+                pos = sprinklers[last].second;
+            }
         }
+        cout << last << " ";
 
-        if (last == 0)
+        if (current == last)
             return -1;
+        current = last;
+        count++;
     }
 
     return count;
 }
 
 
+void print_sprinklers(vector<pair<double, double>> sprinklers, int num_sprinklers) {
+
+    for (int i = 0; i < num_sprinklers; i++)
+        cout << sprinklers[i].first << " ";
+    cout << "\n";
+    for (int i = 0; i < num_sprinklers; i++)
+        cout << sprinklers[i].second << " ";
+    cout << "\n\n";
+}
+
+
 int main() {
-
-
 
     int num_sprinklers;
     double strip_length, strip_width;
     double sprinkler_pos, sprinkler_radius;
-
+    int j = 0;
     while (cin >> num_sprinklers) {
-        cin >> num_sprinklers >> strip_length >> strip_width;
+        cin >> strip_length >> strip_width;
 
         vector<pair<double, double>> sprinklers(num_sprinklers);
+
+        int useless_sprinklers = 0;
 
         for (int i = 0; i < num_sprinklers; i++) {
 
             cin >> sprinkler_pos >> sprinkler_radius;
 
-            sprinklers[i] = get_range(sprinkler_pos, sprinkler_radius, strip_width);
+            if (sprinkler_radius <= strip_width / 2) {
+                useless_sprinklers++;
+                continue;
+            }
+            sprinklers[i - useless_sprinklers] = get_range(sprinkler_pos, sprinkler_radius, strip_width);
         }
 
-        sort(sprinklers.begin(), sprinklers.end());
-
+        num_sprinklers -= useless_sprinklers;
+        sprinklers.resize(num_sprinklers);
+        sort(sprinklers.begin(), sprinklers.end(), compare_start);
         cout << count_sprinklers(sprinklers, num_sprinklers, strip_length) << "\n";
     }
 
